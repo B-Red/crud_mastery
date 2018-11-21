90506d5b0875db463cb618a85ab4c179295e9120
Brandon Redshirt <brandon.redshirt@gmail.com>
2018-11-21 12:42:32 -0700

    put request completed

--- cea_backend/index.js
+++ cea_backend/index.js
@@ -6,7 +6,44 @@ var data = require('./data.json')
 
 const port = 3000
 
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
+    res.send({ data })
+})
+
+app.get('/:id', (req, res) => {
+    var { id } = req.params
+    var filtered = data.filter(obj => {
+        return obj.id == id
+    })
+    res.send ({ data: filtered })
+})
+
+app.post('/', (req, res) => {
+    var { body } = req
+    var obj = {
+        id: data.length + 1,
+        sport: body.sport
+    }
+    data.push( obj )
+    res.json({ data: obj })
+})
+
+app.put('/:id', (req, res) => {
+    var { body } = req
+    var { id } = req.params
+
+    var mapped = data.map(obj => {
+        if(id == obj.id){
+            return obj = {
+                id: obj.id,
+                ...body
+            }
+        }
+        return obj
+    })
+    data = mapped
     res.send({ data })
 })
 
