956aef22903612561dd9ff6c76198cf4906b5df7
Brandon Redshirt <brandon.redshirt@gmail.com>
2018-11-21 10:47:32 -0700

    backend set up

---
+++ cea_backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+var data = require('./data.json')
+
+const port = 3000
+
+
+
+app.listen(port)
